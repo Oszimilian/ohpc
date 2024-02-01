051fe33d4e4a2307a5434e89fcd8ac73a1cc3acb
Oszimilian <maximilian@hoffmann-maximilian.net>
2024-02-01 12:23:39 +0100

    measure time of execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 
 #include "ohpc.h"
@@ -19,7 +20,10 @@ int main() {
 
         ohpc::Ohpc solver;
         //solver.debug("debug.txt");
+        auto start = std::chrono::steady_clock::now();
         auto sol = solver.calc(str);
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds> (end - start).count() << std::endl;
 
         std::cout << sol << std::endl;
 
